7f3df2375f75ad1aa5182e81deecd8eeedad85cc
huynhvuluangpm <luanhv@gpm.vn>
2017-07-27 08:15:22 +0700

    aa

--- config/app.php
+++ config/app.php
@@ -12,7 +12,7 @@ return [
     | any other location as required by the application or its packages.
     */
 
-    'name' => 'Phòng công tác Sinh Viên',
+    'name' => 'GoldStar',
 
     /*
     |--------------------------------------------------------------------------
