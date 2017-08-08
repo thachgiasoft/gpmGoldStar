30f0f0fbce3b2219417e64972f9536adea119e2e
huynhvuluangpm <luanhv@gpm.vn>
2017-08-08 17:35:06 +0700

    ưee

--- BanHang/DanhSachPhieuXuatKhac.aspx.cs
+++ BanHang/DanhSachPhieuXuatKhac.aspx.cs
@@ -22,26 +22,6 @@ namespace BanHang
             data = new dtPhieuXuatKhac();
             gridPhieuXuatKhac.DataSource = data.DanhSachPhieuXuatKhac();
             gridPhieuXuatKhac.DataBind();
-        }
-
-        protected void gridPhieuXuatKhac_RowDeleting(object sender, DevExpress.Web.Data.ASPxDataDeletingEventArgs e)
-        {
-            //int ID = Int32.Parse(e.Keys[0].ToString());
-            //data = new dtPhieuXuatKhac();
-            //DataTable da = data.LayDanhSachChiTietPhieuXuatKhac_ID(ID);
-            //if (da.Rows.Count != 0)
-            //{
-            //    DataRow dr = da.Rows[0];
-            //    int SoLuong = Int32.Parse(dr["SoLuong"].ToString());
-            //    //dtLichSuKho.ThemLichSu(ID, Int32.Parse(Session["IDNhanVien"].ToString()), SoLuong * (-1), "Xóa danh sách phiếu xuất khác");
-            //}
-            //data.XoaPhieuXuatKhac_ID(ID);
-            //e.Cancel = true;
-            //gridPhieuXuatKhac.CancelEdit();
-            //LoadGrid();
-
-            //dtLichSuTruyCap dt1 = new dtLichSuTruyCap();
-            //dtLichSuTruyCap.ThemLichSu(Session["IDNhanVien"].ToString(), Session["IDNhom"].ToString(), "Phiếu Xuất Khác: " + ID,Session["IDKho"].ToString(), "Nhập xuất tồn", "Xóa"); ;   
-        }
+        }  
     }
 }
